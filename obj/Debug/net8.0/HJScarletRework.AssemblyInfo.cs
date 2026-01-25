71afb13ad34e344c8ed0daba155b8f398368e424
Scarlet <143493496+BookshelfScarlett@users.noreply.github.com>
2026-01-26 00:01:03 +0800

    更多宠物贴图，大部分还没正式实装

--- obj/Debug/net8.0/HJScarletRework.AssemblyInfo.cs
+++ obj/Debug/net8.0/HJScarletRework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HJScarletRework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc422ab3fb37b1d018ffe7b06b9ab5b64b13797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5764dcb59ffbc5e77a77dfa9d8d136635e2d77d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HJScarletRework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HJScarletRework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
