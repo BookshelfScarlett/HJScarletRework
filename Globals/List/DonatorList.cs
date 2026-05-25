823b3e4e63e58a2a04dba86b85591e64e9b65972
Scarlet <143493496+BookshelfScarlett@users.noreply.github.com>
2026-05-25 14:39:50 +0800

    ListUpdate

--- Globals/List/DonatorList.cs
+++ Globals/List/DonatorList.cs
@@ -7,13 +7,26 @@ namespace HJScarletRework.Globals.List
     {
         public static List<string> SupporterList=
             [
-                "南鸢北笙",
-                "世纪小花",
-                "Demon Lilies",
-                "溯月Shizuku",
-                "巴雷难破长夜",
                 "樱羽一花",
+                "Demon Lilies",
+                "世纪小花",
+                
+                "溯月Shizuku",
                 "论时间的涟漪",
+                "Woodi Stigma",
+                
+                "锯角",
+                "南鸢北笙",
+                "梦山之主",
+                
+                "巴雷难破长夜",
+                "银九",
+            ];
+        public static List<string> DeveloperList =
+            [
+                "绯色书架Scarlet",
+                "Inoljw",
+                "名字",
                 "芙蕾雅Freya"
             ];
     }
